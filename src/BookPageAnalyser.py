0465639db787633a6c72b3cc058405d3c9730219
jyh <jyh@eastcom.com>
2016-08-02 11:47:41 +0800

    删除：去掉多余的代码。

--- src/BookPageAnalyser.py
+++ src/BookPageAnalyser.py
@@ -3,11 +3,4 @@ __author__ = 'jiangyh'
 from PageAnalyser import PageAnalyser
 
 class BookPageAnalyser(PageAnalyser):
-    def get_book_tilte(self, pagestr):
-        pass
-
-    def get_book_author(self, pagestr):
-        pass
-
-    def get_chapter_info_list(self, url, pagestr):
-        pass
+    pass
