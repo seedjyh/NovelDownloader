e6bb6e567e71c4fcd1d58f05066806dba782c850
jyh <jyh@eastcom.com>
2016-08-02 11:47:16 +0800

    修改：SilukeBookPageAnalyser在构造时就读入页面URL和内容。TestSilukeBookPageAnalyser也做相应修改。

--- src/SilukeBookPageAnalyser.py
+++ src/SilukeBookPageAnalyser.py
@@ -6,16 +6,20 @@ from ChapterInfo import ChapterInfo
 from UrlHandle import UrlHandle
 
 class SilukeBookPageAnalyser(BookPageAnalyser):
-    def get_book_title(self, pagestr):
-        parser = TextParser(pagestr)
+    def __init__(self, url, pagestr):
+        self.__url = url
+        self.__pagestr = pagestr
+
+    def get_book_title(self):
+        parser = TextParser(self.__pagestr)
         return parser.JumpStr('<h1>', 1).JumpStr('>', 1).GetStr('<').decode('utf-8').encode('gbk')
 
-    def get_book_author(self, pagestr):
-        parser = TextParser(pagestr)
+    def get_book_author(self):
+        parser = TextParser(self.__pagestr)
         return parser.JumpStr('<span>', 2).JumpStr('<strong>').GetStr('<').decode('utf-8').encode('gbk')
 
-    def get_chapter_info_list(self, url, pagestr):
-        parser = TextParser(self.__GetPagePartContainsChapterInformation(pagestr))
+    def get_chapter_info_list(self):
+        parser = TextParser(self.__GetPagePartContainsChapterInformation())
         chapter_info_list = list()
         while (True):
             try:
@@ -23,9 +27,9 @@ class SilukeBookPageAnalyser(BookPageAnalyser):
             except NoSuchKeywordException:
                 break
             title = parser.JumpStr('>').GetStr('<').decode('utf-8').encode('gbk')
-            chapter_info_list.append(ChapterInfo(UrlHandle(url).GetHost() + relative_url, title))
+            chapter_info_list.append(ChapterInfo(UrlHandle(self.__url).GetHost() + relative_url, title))
         return chapter_info_list
 
-    def __GetPagePartContainsChapterInformation(self, pagestr):
-        parser = TextParser(pagestr)
+    def __GetPagePartContainsChapterInformation(self):
+        parser = TextParser(self.__pagestr)
         return parser.JumpStr('class="booklist"').GetStr('</div>')
--- src/TestSilukeBookPageAnalyser.py
+++ src/TestSilukeBookPageAnalyser.py
@@ -9,19 +9,18 @@ class TestSilukeBookPageAnalyser(unittest.TestCase):
     def setUp(self):
         self.__url = 'http://www.siluke.tw/ny10354/'
         self.__pagestr = self.__LoadFile('test_data/siluke_book_page.html')
-        pass
 
     def test_get_book_title(self):
-        analyser = SilukeBookPageAnalyser()
-        self.assertEqual('全职高手', analyser.get_book_title(self.__pagestr))
+        analyser = SilukeBookPageAnalyser(self.__url, self.__pagestr)
+        self.assertEqual('全职高手', analyser.get_book_title())
 
     def test_get_book_author(self):
-        analyser = SilukeBookPageAnalyser()
-        self.assertEqual('蝴蝶蓝', analyser.get_book_author(self.__pagestr))
+        analyser = SilukeBookPageAnalyser(self.__url, self.__pagestr)
+        self.assertEqual('蝴蝶蓝', analyser.get_book_author())
 
     def test_get_chapter_info_list(self):
-        analyser = SilukeBookPageAnalyser()
-        info_list = analyser.get_chapter_info_list(self.__url, self.__pagestr)
+        analyser = SilukeBookPageAnalyser(self.__url, self.__pagestr)
+        info_list = analyser.get_chapter_info_list()
         self.assertEqual(1781, len(info_list))
         self.assertEqual('http://www.siluke.tw/ny10354/6071434.html', info_list[0].url())
         self.assertEqual('http://www.siluke.tw/ny10354/6071435.html', info_list[1].url())
