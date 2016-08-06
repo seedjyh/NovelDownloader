2fd1be96895fec5a6bb93f464cd9d3f21ba39eb2
seedjyh <seedjyh@gmail.com>
2016-08-06 10:20:35 +0800

    修改：允许使用参数-u来指定小说地址。目前只支持www.siluke.tw。

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,28 @@
 __author__ = 'jiangyh'
 
 from SilukeNovelDownloader import SilukeNovelDownloader
+import sys, getopt
+
+def ReadArguments(sysargv):
+    novel_book_url = ''
+    output_file_path = ''
+    opts, args = getopt.getopt(sysargv[1:], "u:w:")
+    for op, value in opts:
+        if op == '-u':
+            novel_book_url = value
+        elif op == '-w':
+            output_file_path = value
+        else:
+            print('Unknown argument: ', op)
+    return novel_book_url, output_file_path
 
 if __name__ == '__main__':
+    url, output_file = ReadArguments(sys.argv)
+    if url == '':
+        print("No valid URL. Use '-u' to set it.")
+        exit()
     downloader = SilukeNovelDownloader()
-    book = downloader.DownloadBook('http://www.siluke.tw/ny10354/')
-    #book = downloader.DownloadBook('http://www.siluke.tw/ny26173/')
-    book.WriteToFile(book.title() + '.txt')
+    book = downloader.DownloadBook(url)
+    if output_file == '':
+        output_file_path = book.title() + '.txt'
+    book.WriteToFile(output_file)
